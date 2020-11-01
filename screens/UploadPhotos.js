e2a79374181b2790c110bd9d13bdbf2569c07aca
Sagar Parker <sagar8parker@gmail.com>
2020-11-01 12:57:13 +0530

    Immagine basic file structure

---
+++ screens/UploadPhotos.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function UploadPhotos() {
+    return (
+        
+            <View style={styles.container}>
+                <Text>Upload screen</Text>
+            </View>
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        padding:24;
+    }
+})
